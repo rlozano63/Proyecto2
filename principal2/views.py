f5870f82b611239c75dec824d11283585a180715
rlozano63 <riquelme.richar@gmail.com>
2016-11-07 00:08:40 -0500

    Update views.py

--- principal2/views.py
+++ principal2/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponse 
+from django.template import loader, Context
 from principal2.models import usuario
 
 # Create your views here.
 
 def lista_usuarios(request):
 	usuarios = usuario.objects.all()
-	return render_to_response('lista_usuarios.html',{'lista':usuarios})
+	mi_template = loader.get_template("lista_usuarios.html")
+	mi_contexto = Context({"usuarios":usuarios})
+	return HttpResponse(mi_template(mi_contexto))
+
+	#return render_to_response('lista_usuarios.html',{'lista':usuario})
